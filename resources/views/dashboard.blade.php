14c782f686a19829b7850cadbe0dc2843002b8d6
ms-online <27732357@qq.com>
2021-11-10 17:52:28 +0800

    lesson-38

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,31 +7,35 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">序号</th>
-                            <th scope="col">用户姓名</th>
-                            <th scope="col">用户邮箱</th>
-                            <th scope="col">创建时间</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php($i = 1)
-                        @foreach ($users as $user)
-                        <tr>
-                            <th scope="row">{{ $i++ }}</th>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->email }}</td>
-                            <td>{{ Carbon\Carbon::parse($user->created_at)->diffForHumans() }}</td>
-                        </tr>
-                        @endforeach
+        <div class="container">
+            <div class="card">
+                <div class="card-header">所有用户</div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">序号</th>
+                                <th scope="col">用户姓名</th>
+                                <th scope="col">用户邮箱</th>
+                                <th scope="col">创建时间</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php($i = 1)
+                            @foreach ($users as $user)
+                            <tr>
+                                <th scope="row">{{ $i++ }}</th>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ Carbon\Carbon::parse($user->created_at)->diffForHumans() }}</td>
+                            </tr>
+                            @endforeach
 
 
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         </div>
     </div>
