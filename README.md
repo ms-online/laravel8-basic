e35ef96fe7554d3ba7b15b39aaa118748b2116cd
ms-online <27732357@qq.com>
2021-11-11 18:49:54 +0800

    lesson-40

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-lesson-40:准备页面模版
-
-免费页面模版：
-
-https://bootstrapmade.com/
+lesson-40:路由守卫
--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
+    //路由守卫
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         return view('contact');
